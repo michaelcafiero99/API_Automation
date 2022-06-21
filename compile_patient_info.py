19d8dc86d4efa0559f5ebfdfff90d707e687137b
michaelcafiero99 <78000917+michaelcafiero99@users.noreply.github.com>
2022-06-21 15:55:56 -0500

    Input functionality

--- compile_patient_info.py
+++ compile_patient_info.py
@@ -2,57 +2,79 @@
 # facilityid:81592725 -- get the home address of the  patient
 # facilitynote:elit
 # futuresubmitdate:2022-06-05 -- The date the order should be sent. Defaults to today.
-# highpriority:true -- If true, then the order should be sent STAT. loinc:incididunt officia dolor
+# highpriority:true -- If true, then the order should be sent STAT. default = false.
 # loinc:incididunt officia dolor -- The LOINC of the lab you wish to order.
 # ordertypeid:18766226 -- The athena ID of the lab to order. Get the IDs using /reference/order/lab. Either this or
-# LOINC can be used, but not both.
+# LOINC can be used, but not both
 # providernote -- will need to be spoken with the order. Default = empty
+
+
 import pprint
+import patient_info_call
 from find_conditions_call import find_conditions
 from patient_info_call import patient_info
 import json
-
 # athena's sample information:
 
-patient_id = '0'
-practice_id = '195900'
-department_id = '1'
-# patient_info1 = patient_info(practice_id, department_id, patient_id)
-patient_info = json.loads(patient_info(practice_id, department_id, patient_id))
-patient_conditions = json.loads(find_conditions(practice_id, department_id, patient_id))
-#  get all active conditions, and patient location.
 
-json1 = json.dumps((patient_info['entry']), sort_keys=True, indent=4)
-dict2 = json.loads(json1)
-address_info = (((dict2[0])['resource'])['address'])[0]
-patient_city = address_info['city']
-patient_country = address_info['country']
-patient_line = address_info['line']
-patient_zip = address_info['postalCode']
-patient_state = address_info['state']
-
-print("The patient lives at the address of " + patient_line[1] + ", " + patient_city + " "
-      + patient_state + ", " + patient_country + ", " + patient_zip)
-condition_dict = {}
-for condition in patient_conditions['entry']:
-    resource = condition['resource']
-    status = resource['clinicalStatus']
-    id = resource['id']
-    code = resource['code']
-    name = code['text']
-    exists = False
-    if name in condition_dict.values():
-        exists = True
-
-    if status == 'active' and not exists:
-        condition_dict.update({id: name})
-pp = pprint.PrettyPrinter(width=55, compact=True)
-pp.pprint(condition_dict)
-if len(condition_dict) > 20:
-    print("Wow, this patient has a lot of problems! Notify the doctor that ordering tests won't be needed,"
-          " they will be dead soon. ")
+def receive_order(input_string, patient_id, department_id, practice_id):
+    # example: "order lipid panel"
+    test_order = input_string[5:]
+    priority = "default"
+    if test_order.contains("stat"):
+        priority = "STAT"
+        test_order = test_order[0:len(test_order) - 4]
+    compile_info(patient_id, practice_id, department_id, test_order, "today", priority)
 
 
-# print((patient_conditions['entry'][0]))
+def compile_info(patient_id, practice_id, department_id, test, futuresubmitdate, priority):
+    #  TODO: Identify the lab LOINC or order type based on the string input
+    patient_id = patient_id
+    practice_id = practice_id
+    department_id = department_id
+    # patient_info1 = patient_info(practice_id, department_id, patient_id)
+    patient_info = json.loads(patient_info_call.patient_info(practice_id, department_id, patient_id))
+    patient_conditions = json.loads(find_conditions(practice_id, department_id, patient_id))
+    #  get all active conditions, and patient location.
 
-#  print(patient_conditions)
+    json1 = json.dumps((patient_info['entry']), sort_keys=True, indent=4)
+    dict2 = json.loads(json1)
+    address_info = (((dict2[0])['resource'])['address'])[0]
+    patient_city = address_info['city']
+    patient_country = address_info['country']
+    patient_line = address_info['line']
+    patient_zip = address_info['postalCode']
+    patient_state = address_info['state']
+
+    print("The patient lives at the address of " + patient_line[1] + ", " + patient_city + " "
+          + patient_state + ", " + patient_country + ", " + patient_zip)
+    condition_dict = {}
+    for condition in patient_conditions['entry']:
+        resource = condition['resource']
+        status = resource['clinicalStatus']
+        id = resource['id']
+        code = resource['code']
+        name = code['text']
+        exists = False
+        if name in condition_dict.values():
+            exists = True
+
+        if status == 'active' and not exists:
+            condition_dict.update({id: name})
+    pp = pprint.PrettyPrinter(width=55, compact=True)
+    pp.pprint(condition_dict)
+    if len(condition_dict) > 20:
+        print("Wow, this patient has a lot of problems! Notify the doctor that ordering tests won't be needed,"
+              " they will be dead soon. ")
+    # POST orders with given info
+
+
+inputlist = []
+message_array = ["Please enter a patient id", "Please enter a practice id", "Please enter a department id",
+                 "What test will you be ordering?",
+                 "Will it be submitted at a future date?", "What is the priority of your test?"]
+for i in range(6):
+    inputs = input(message_array[i])
+    inputlist.append(inputs)
+
+compile_info(inputlist[0], inputlist[1], inputlist[2], inputlist[3], inputlist[4], inputlist[5])
--- token_call.py
+++ token_call.py
@@ -1,53 +1,70 @@
 import requests
 import requests.auth
-from datetime import datetime
+import sqlparse
+import datetime
+from datetime import timedelta
 from database_functions import create_db_connection, read_query, execute_query
+
+
 # ‘1753-01-01 00:00:00’
 # TODO: track if the token is active -- it expires after 1 hour
 
+def new_token(connection):
+
+    url = "https://api.preview.platform.athenahealth.com/oauth2/v1/token"
+    payload = 'grant_type=client_credentials&scope=athena%2Fservice%2FAthenanet.MDP.*%0A'
+    headers = {
+        'data': 'grant_type=client_credentials',
+        'Cache-Control': 'no-cache',
+        'Authorization': 'Basic MG9hZGIxM3ZqODBtaHhhQXgyOTc6TXF4U3F5VlktemlwdXR0cDJYNVRILW9zUzI1cWNQVjJXa0tEZ3RKQg==',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'dtCookie=5AF86EC06F3D5BB6E6D09A1BBF3C7710|RUM+Default+Application|1'
+    }
+
+    token_response = requests.request("POST", url, headers=headers, data=payload)
+    content = token_response.content
+    #  print(content)
+    decoded_string = content.decode()
+    token_string = decoded_string[59:87]
+    auth_string = "Bearer " + token_string + ""
+    now = datetime.datetime.now()
+    now_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    expiration = now + timedelta(hours=1)
+    expiration_string = expiration.strftime("%Y-%m-%d %H:%M:%S")
+    q2 = """INSERT
+           INTO SpeakIT.tokens (token_id, creation_time,expiration_time,token_content)
+           VALUES(4,""" + "'" + now_string + "'" + "," + "'" + expiration_string + "'" + "," + "'" + auth_string + "'" + ")"""
+    results = execute_query(connection, q2)
+    #print(results)
+    q3 = "SELECT * FROM SpeakIT.tokens;"
+    results2 = read_query(connection, q3)
+    #print(results2)
+
+
+
+def check_token(connection):
+
+    q3 = "SELECT * FROM SpeakIT.tokens;"
+    results2 = read_query(connection, q3)
+    print(results2)
+    parsed = ((results2[0])[2])
+    now = datetime.datetime.now()
+
+    if now > parsed:
+        new_token(connection)
+
 
 def get_token():
-    now = datetime.now()
 
-    now_string = now.strftime("%Y-%m-%d")
-
-    auth_string = ""
-    q1 = """
-    SELECT *
-    FROM tokens;
-    """
     pw = "punkinbunker1P!"
     db = "SpeakIT"
     connection = create_db_connection("localhost", "root", pw, db)
-    #results = read_query(connection, q1)
-    if True:
+    check_token(connection)
 
-        #print(results)
-        url = "https://api.preview.platform.athenahealth.com/oauth2/v1/token"
-        payload = 'grant_type=client_credentials&scope=athena%2Fservice%2FAthenanet.MDP.*%0A'
-        headers = {
-            'data': 'grant_type=client_credentials',
-            'Cache-Control': 'no-cache',
-            'Authorization': 'Basic MG9hZGIxM3ZqODBtaHhhQXgyOTc6TXF4U3F5VlktemlwdXR0cDJYNVRILW9zUzI1cWNQVjJXa0tEZ3RKQg==',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'dtCookie=5AF86EC06F3D5BB6E6D09A1BBF3C7710|RUM+Default+Application|1'
-        }
-
-        token_response = requests.request("POST", url, headers=headers, data=payload)
-        content = token_response.content
-        #  print(content)
-        decoded_string = content.decode()
-        token_string = decoded_string[59:87]
-        auth_string = "Bearer " + token_string + ""
-        q2 = """INSERT IGNORE
-        INTO SpeakIT.tokens (token_id, creation_time,expiration_time,token_content)
-        VALUES(1,""" + now_string + "," + now_string + "," + auth_string + ")"""
-        results = read_query(connection, q2)
-        #print(q2)
-        q3 = "SELECT * FROM SpeakIT.tokens;"
-        results2 = read_query(connection, q3)
-
+    q3 = "SELECT * FROM SpeakIT.tokens;"
+    results2 = read_query(connection, q3)
+    auth_string = ((results2[0])[3])
+    print(auth_string)
     return auth_string
 
 
-#get_token()
